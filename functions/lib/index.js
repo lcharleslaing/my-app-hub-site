8d690bece1a66ccec7ff0fc10eda3fa8dadf644f
lcharleslaing <lcharleslaing@gmail.com>
2025-02-23 14:31:39 -0500

    added emailing functionality for invitations

---
+++ functions/lib/index.js
@@ -0,0 +1,43 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sendInvitationEmail = void 0;
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const nodemailer = require("nodemailer");
+admin.initializeApp();
+// Configure nodemailer with your email service (e.g., Gmail, SendGrid)
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: functions.config().email.user,
+        pass: functions.config().email.password
+    }
+});
+exports.sendInvitationEmail = functions.firestore
+    .document('invitations/{invitationId}')
+    .onCreate(async (snap, context) => {
+    const invitation = snap.data();
+    const registrationLink = `${functions.config().app.url}/register?token=${invitation.token}`;
+    const mailOptions = {
+        from: functions.config().email.user,
+        to: invitation.email,
+        subject: 'Invitation to Join MyAppHub',
+        html: `
+        <h2>Welcome to MyAppHub!</h2>
+        <p>You have been invited to join MyAppHub with the role of ${invitation.role}.</p>
+        <p>Click the link below to complete your registration:</p>
+        <a href="${registrationLink}">${registrationLink}</a>
+        <p>This invitation will expire in 7 days.</p>
+      `
+    };
+    try {
+        await transporter.sendMail(mailOptions);
+        console.log(`Invitation email sent to ${invitation.email}`);
+        return null;
+    }
+    catch (error) {
+        console.error('Error sending email:', error);
+        throw new functions.https.HttpsError('internal', 'Error sending invitation email');
+    }
+});
+//# sourceMappingURL=index.js.map
